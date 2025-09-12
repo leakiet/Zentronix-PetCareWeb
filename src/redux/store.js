1cd999f02163369591857f8a0f2c984f45566611
TrungLeThanh2005 <lethanhtrung28042000@gmail.com>
2025-09-12 11:34:04 +0700

    Update UI/UX for chatbot

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { employeeReducer } from './user/employeeSlice.js'
 import { customerReducer } from './user/customerSlice.js'
 import { cartReducer } from './cart/cartSlice.js'
+import { conversationReducer } from './conversation/conversationSlice.js'
 import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
@@ -9,13 +10,14 @@ import storage from 'redux-persist/lib/storage'
 const rootPersistConfig = {
   key: 'root',
   storage: storage,
-  whitelist: ['employee', 'customer', 'cart']
+  whitelist: ['employee', 'customer', 'cart', 'conversation']
 }
 
 const rootReducer = combineReducers({
   employee: employeeReducer,
   customer: customerReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  conversation: conversationReducer
 })
 
 const persistedReducer = persistReducer(rootPersistConfig, rootReducer)
